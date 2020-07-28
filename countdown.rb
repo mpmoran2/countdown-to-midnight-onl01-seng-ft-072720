c66069d470c170d4fea0f4414ec7d579967eb6df
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 23:10:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
-
 def countdown
+  n = 10
+    while n > 1
+    puts "#{number} SECOND(S)!"
+    n -= 1
+  end
+end
+
+puts "HAPPY NEW YEAR!"
+
+def countdown_with_sleep
+  sleep 1.seconds 
+  until Time.now > time + 5.seconds
 end
